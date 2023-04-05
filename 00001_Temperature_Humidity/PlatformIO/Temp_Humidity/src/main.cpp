f612c7508669059d89e1b292f006f0126c830812
Javier Fernandez Martinez <jafema@users.noreply.github.com>
2023-04-06 01:16:03 +0200

    Fibebase App sensor DTH11 reading
     *    ESP32 Data Logging to Firebase Realtime Database
     *    https://randomnerdtutorials.com/esp32-data-logging-firebase-realtime-database/
     *
     *    ESP32/ESP8266: Firebase Web App to Display Sensor Readings (with Authentication)
     *    https://randomnerdtutorials.com/esp32-esp8266-firebase-web-app-sensor/
     *
     *    ESP32/ESP8266: Firebase Data Logging Web App (Gauges, Charts, and Table)
     *    https://randomnerdtutorials.com/esp32-esp8266-firebase-gauges-charts/

--- 00001_Temperature_Humidity/PlatformIO/Temp_Humidity/src/main.cpp
+++ 00001_Temperature_Humidity/PlatformIO/Temp_Humidity/src/main.cpp
@@ -23,6 +23,16 @@
  *    https://randomnerdtutorials.com/esp32-firebase-web-app/#install-firebase-tools
  * 
  *    authDomain: esp32-firebase-demo-e1d9f.firebaseapp.com
+ * 
+ *    ESP32 Data Logging to Firebase Realtime Database
+ *    https://randomnerdtutorials.com/esp32-data-logging-firebase-realtime-database/
+ *    
+ *    ESP32/ESP8266: Firebase Web App to Display Sensor Readings (with Authentication)
+ *    https://randomnerdtutorials.com/esp32-esp8266-firebase-web-app-sensor/
+ *    
+ *    ESP32/ESP8266: Firebase Data Logging Web App (Gauges, Charts, and Table)
+ *    https://randomnerdtutorials.com/esp32-esp8266-firebase-gauges-charts/
+ * 
  *  Board:
  *     ESP32 Dev Moduler
  *     https://www.az-delivery.de/en/blogs/azdelivery-blog-fur-arduino-und-raspberry-pi/esp32-jetzt-mit-boardverwalter-installieren
@@ -45,9 +55,11 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 
-
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
+
+#include <Wire.h>
+#include "time.h"
 
 // Provide the token generation process info.
 #include "addons/TokenHelper.h"
@@ -55,35 +67,47 @@
 #include "addons/RTDBHelper.h"
 
 // Insert your network credentials
-#define WIFI_SSID "" //"REPLACE_WITH_YOUR_SSID"
-#define WIFI_PASSWORD "" //"REPLACE_WITH_YOUR_PASSWORD"
+#define WIFI_SSID //"REPLACE_WITH_YOUR_SSID"
+#define WIFI_PASSWORD //"REPLACE_WITH_YOUR_PASSWORD"
 
 // Insert Firebase project API Key
-#define API_KEY "" //"REPLACE_WITH_YOUR_FIREBASE_PROJECT_API_KEY"
+#define API_KEY //"REPLACE_WITH_YOUR_FIREBASE_PROJECT_API_KEY"
 
 // Insert RTDB URLefine the RTDB URL */
-#define DATABASE_URL "" //"REPLACE_WITH_YOUR_FIREBASE_DATABASE_URL" 
+#define DATABASE_URL  //"REPLACE_WITH_YOUR_FIREBASE_DATABASE_URL" L" 
+
+// Insert Authorized Email and Corresponding Password
+#define USER_EMAIL  //"REPLACE_WITH_THE_USER_EMAIL"
+#define USER_PASSWORD //"REPLACE_WITH_THE_USER_PASSWORD"
 
 
 /**
  * @brief Define Firebase Data Objetc 
  * 
  */
-
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-unsigned long sendDataPrevMillis = 0;
-int count = 0;
-bool signupOK = false;
+// Variable to save USER ID
+String uid;
 
+// Database main path (to be updated in the setup with the user ID)
+String databasePath;
+// Database child nodes
+String tempPath = "/temperature";
+String humPath = "/humidity";
+String timePath = "/timestamp";
 
-/**
- * @brief Sensor DHT Configuration
- * 
- */
+// Parent Node (to be updated in every loop)
+String parentPath;
 
+int timestamp;
+FirebaseJson json;
+
+const char* ntpServer = "pool.ntp.org";
+
+//DTH11 sensor
 #define DHTPIN 13 /* Digital pin connected to the DHT sensor */
 
 /* Uncommented the type of sensor in use: */
@@ -93,8 +117,31 @@ bool signupOK = false;
 
 DHT dht(DHTPIN, DHTTYPE);
 
+float temperature;
+float humidity;
 
-// board = az-delivery-devkit-v4
+// Timer variables (send new readings every three minutes)
+unsigned long sendDataPrevMillis = 0;
+unsigned long timerDelay = 180000;
+
+// Initialize DTH11
+void initDHT11(){
+  dht.begin();
+}
+
+// Initialize WiFi
+void initWiFi(){
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+}
+
 
 String readDHTTemperature() {
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
@@ -117,86 +164,81 @@ String readDHTTemperature() {
   return retVal;
 }
 
-
+// Function that gets current epoch time
+unsigned long getTime() {
+  time_t now;
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    //Serial.println("Failed to obtain time");
+    return(0);
+  }
+  time(&now);
+  return now;
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  // WiFi connection
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
+  // Initialize sensor
+  initDHT11();
+  initWiFi();
+  configTime(0, 0, ntpServer);
 
-  // Firebase
-  /* Assign the api key (required) */
+  // Assign the api key (required)
   config.api_key = API_KEY;
 
-  /* Assing the RTDB URL (required) */
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  // Assign the RTDB URL (required)
   config.database_url = DATABASE_URL;
 
-  /* Sing up */
-  if (Firebase.signUp(&config, &auth, "","")){
-    Serial.println("ok");
-    signupOK = true;
-  }
-  else{
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
-  }
-
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
-  
-  Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
 
+  // Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
 
-  // Sensor DHT init
-  dht.begin();
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  // Initialize the library with the Firebase authen and config
+  Firebase.begin(&config, &auth);
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(uid);
+
+  // Update database path
+  databasePath = "/UsersData/" + uid + "/readings";
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-/*   // Wait a few seconds between measurements.
-  delay(2000);
-  
-  Serial.println(F("Temperature [ºC]: "));
-  readDHTTemperature();
- */
-
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+  // Send new readings to database
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
     sendDataPrevMillis = millis();
-    // Write an Int number on the database path test/int
-    if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-    }
-    else {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
-    count++;
-    
-    // Write an Float number on the database path test/float
-    if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-    }
-    else {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
+
+    //Get current timestamp
+    timestamp = getTime();
+    Serial.print ("time: ");
+    Serial.println (timestamp);
+
+    parentPath= databasePath + "/" + String(timestamp);
+
+    json.set(tempPath.c_str(), String(dht.readTemperature()));
+    json.set(humPath.c_str(), String(dht.readHumidity()));
+    json.set(timePath, String(timestamp));
+    Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
   }
 
 }
